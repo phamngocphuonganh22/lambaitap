9365fe19781a07fc90740f091f7de95daa9dc046
phamngocphuonganh22 <114391167+phamngocphuonganh22@users.noreply.github.com>
2023-04-24 11:15:13 +0700

    Add files via upload

---
+++ incactu1denN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class incactu1denN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n ;
+        System.out.println("nhap so: ");
+        n = scanner.nextInt();
+        for(int i =1; i<= n; i++){
+            System.out.println("hien thi so: " + i );
+        }
+    }
+}
