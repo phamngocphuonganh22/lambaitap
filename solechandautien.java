3c656bd42e7d997b30684882f6fd9c7b146c6a8b
phamngocphuonganh22 <114391167+phamngocphuonganh22@users.noreply.github.com>
2023-04-27 14:53:36 +0700

    Add files via upload

---
+++ solechandautien.java
@@ -0,0 +1,9 @@
+public class solechandautien {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i<20){
+            System.out.println(i);
+            i+=2;
+        }
+    }
+}
