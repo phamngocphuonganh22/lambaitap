f24a7482fd7a8afbc3a4907f5ccbdb7372426b79
phamngocphuonganh22 <114391167+phamngocphuonganh22@users.noreply.github.com>
2023-05-08 16:17:37 +0700

    Add files via upload

---
+++ hienthikytutrongchuoi.java
@@ -0,0 +1,8 @@
+public class hienthikytutrongchuoi {
+    public static void main(String[] args) {
+        String hihi = "helo am diu"; //nhap chuoi
+        for (int i = 0; i <= hihi.length(); i++) {//dung vong lap for de duyet chuoi
+            System.out.println(hihi.charAt(i)); //su dung ham chatAt de in ra tung ky tu trong chuoi
+        }
+    }
+}
